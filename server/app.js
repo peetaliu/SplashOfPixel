856100907358197e62cf38f4acd48978690cfd3e
Peter <peterdotliu@gmail.com>
2020-09-15 23:01:01 -0400

    Fix code

--- server/app.js
+++ server/app.js
@@ -1,12 +1,13 @@
 const express = require('express')
+const cors = require('cors')
 const unsplashRouter = require('./controllers/unsplash')
 require('express-async-errors')
 const app = express()
 
 app.use(cors())
 app.use(express.static('build'))
-app.use(express.json)
+app.use(express.json())
 
-app.use('/splash', unsplashRouter)
+app.use('/', unsplashRouter)
 
 module.exports = app
