2a867c894e6485cc24671134a9d716a7cc63e884
Peter <peterdotliu@gmail.com>
2020-09-15 23:03:40 -0400

    Implement config for unsplash api, add route for photo search

--- server/controllers/unsplash.js
+++ server/controllers/unsplash.js
@@ -1,9 +1,19 @@
 const unsplashRouter = require('express').Router()
-const axios = require('axios')
 require('express-async-errors')
+const fetch = require('node-fetch')
+global.fetch = fetch
+const Unsplash = require('unsplash-js').default
+const toJson = require('unsplash-js').toJson
+const config = require('../utils/config')
+const unsplash = new Unsplash({ accessKey: config.ACCESS_KEY })
 
+//Search for photos based on params set by user
 unsplashRouter.post('/', async (req, res) => {
-  const body = request.body
+  const body = req.body
+  console.log('body', body)
+  const photos = await unsplash.search.photos(`${body.search}`)
+  res.json(await toJson(photos))
+  console.log(photos.headers)
 })
 
 module.exports = unsplashRouter
