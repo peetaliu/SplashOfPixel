3551533e9da1fbb99a4014fc4ca23ff9b514415a
Peter <peterdotliu@gmail.com>
2020-09-18 23:31:53 -0400

    Add endpoint for returning random stock photo

--- server/controllers/unsplash.js
+++ server/controllers/unsplash.js
@@ -10,10 +10,29 @@ const unsplash = new Unsplash({ accessKey: config.ACCESS_KEY })
 //Search for photos based on params set by user
 unsplashRouter.post('/', async (req, res) => {
   const body = req.body
-  console.log('body', body)
   const photos = await unsplash.search.photos(`${body.search}`)
   res.json(await toJson(photos))
   console.log(photos.headers)
+})
+
+//Return a single random stock photo based on params set by user
+unsplashRouter.get('/random', async (req, res) => {
+  const params = req.query
+  const q = { query: params.s }
+  const qp = `&auto=format${params.w ? `&w=${params.w}` : ''}${
+    params.h ? `&h=${params.h}` : ''
+  }&fit=clip`
+  if (!q.query) {
+    const photo = await toJson(await unsplash.photos.getRandomPhoto())
+    res.send(`<img src=${photo.urls.regular}/>`)
+  } else {
+    const photo = await toJson(await unsplash.photos.getRandomPhoto(q))
+    if (params.w || params.h) {
+      res.send(`<img src=${photo.urls.raw + qp}/>`)
+    } else {
+      res.send(`<img src=${photo.urls.regular}/>`)
+    }
+  }
 })
 
 module.exports = unsplashRouter
